ac8d3c7607a6fd01547afe950b9501761841e88f
Артём Куликов <artiom.tiapkov@mail.ru>
2024-05-19 12:07:49 +0400

    Добился шифрования в соответствии с ГОСТом

--- CudaRuntime1/kernel.cu
+++ CudaRuntime1/kernel.cu
@@ -9,16 +9,14 @@
 #include <Windows.h>
 #include <iostream>
 #include <cooperative_groups.h>
-
+#include <vector>
+#include <random>
+#include <chrono>
 
 
 union half_block_t {
     uint8_t bytes[4];
     uint32_t uint;
-    __host__ __device__ half_block_t invert_bytes_order() const{
-        half_block_t result = {bytes[3], bytes[2], bytes[1], bytes[0]};
-        return result;
-    }
 };
 
 using key_t = half_block_t;
@@ -32,6 +30,7 @@ struct round_key_set {
 };
 
 union block_t {
+    uint64_t ull;
     struct {
         half_block_t lo, hi;
     };
@@ -39,12 +38,12 @@ union block_t {
 };
 
 std::ostream& operator << (std::ostream& s, const block_t& block) {
-    for (int i = sizeof(block_t)-1; i >=0; --i) {
+    for (int i = 0; i < sizeof(block_t); ++i) {
         uint8_t high, low;
         high = block.bytes[i] >> 4;
         low = block.bytes[i] & 0xf;
-        s << (char)(high < 10 ? ('0' + high) : ('A' + high - 10));
-        s << (char)(low < 10 ? ('0' + low) : ('A' + low - 10));
+        s << (high < 10 ? ('0' + high) : ('A' + high));
+        s << (low < 10 ? ('0' + low) : ('A' + low));
     }
     return s;
 }
@@ -58,7 +57,7 @@ void cudaCheck__(cudaError_t e, const char* file, int line) {
 
 #define cudaCheck(e) cudaCheck__((e), __FILE__, __LINE__)
 
-__device__ const unsigned char table[8][16] = {
+__device__ const uint8_t table[8][16] = {
     {12, 4, 6, 2, 10, 5, 11, 9, 14, 8, 13, 7, 0, 3, 15, 1},
     {6, 8, 2, 3, 9, 10, 5, 12, 1, 14, 4, 7, 11, 13, 0, 15},
     {11, 3, 5, 8, 2, 15, 10, 13, 14, 1, 7, 4, 12, 9, 6, 0},
@@ -69,17 +68,34 @@ __device__ const unsigned char table[8][16] = {
     {1, 7, 14, 13, 0, 5, 8, 3, 4, 15, 10, 6, 9, 12, 11, 2}
 };
 
+__device__ const uint8_t table4x256[4][256] = {
+{ 108, 100, 102, 98, 106, 101, 107, 105, 110, 104, 109, 103, 96, 99, 111, 97, 140, 132, 134, 130, 138, 133, 139, 137, 142, 136, 141, 135, 128, 131, 143, 129, 44, 36, 38, 34, 42, 37, 43, 41, 46, 40, 45, 39, 32, 35, 47, 33, 60, 52, 54, 50, 58, 53, 59, 57, 62, 56, 61, 55, 48, 51, 63, 49, 156, 148, 150, 146, 154, 149, 155, 153, 158, 152, 157, 151, 144, 147, 159, 145, 172, 164, 166, 162, 170, 165, 171, 169, 174, 168, 173, 167, 160, 163, 175, 161, 92, 84, 86, 82, 90, 85, 91, 89, 94, 88, 93, 87, 80, 83, 95, 81, 204, 196, 198, 194, 202, 197, 203, 201, 206, 200, 205, 199, 192, 195, 207, 193, 28, 20, 22, 18, 26, 21, 27, 25, 30, 24, 29, 23, 16, 19, 31, 17, 236, 228, 230, 226, 234, 229, 235, 233, 238, 232, 237, 231, 224, 227, 239, 225, 76, 68, 70, 66, 74, 69, 75, 73, 78, 72, 77, 71, 64, 67, 79, 65, 124, 116, 118, 114, 122, 117, 123, 121, 126, 120, 125, 119, 112, 115, 127, 113, 188, 180, 182, 178, 186, 181, 187, 185, 190, 184, 189, 183, 176, 179, 191, 177, 220, 212, 214, 210, 218, 213, 219, 217, 222, 216, 221, 215, 208, 211, 223, 209, 12, 4, 6, 2, 10, 5, 11, 9, 14, 8, 13, 7, 0, 3, 15, 1, 252, 244, 246, 242, 250, 245, 251, 249, 254, 248, 253, 247, 240, 243, 255, 241 },
+{ 203, 195, 197, 200, 194, 207, 202, 205, 206, 193, 199, 196, 204, 201, 198, 192, 139, 131, 133, 136, 130, 143, 138, 141, 142, 129, 135, 132, 140, 137, 134, 128, 43, 35, 37, 40, 34, 47, 42, 45, 46, 33, 39, 36, 44, 41, 38, 32, 27, 19, 21, 24, 18, 31, 26, 29, 30, 17, 23, 20, 28, 25, 22, 16, 219, 211, 213, 216, 210, 223, 218, 221, 222, 209, 215, 212, 220, 217, 214, 208, 75, 67, 69, 72, 66, 79, 74, 77, 78, 65, 71, 68, 76, 73, 70, 64, 251, 243, 245, 248, 242, 255, 250, 253, 254, 241, 247, 244, 252, 249, 246, 240, 107, 99, 101, 104, 98, 111, 106, 109, 110, 97, 103, 100, 108, 105, 102, 96, 123, 115, 117, 120, 114, 127, 122, 125, 126, 113, 119, 116, 124, 121, 118, 112, 11, 3, 5, 8, 2, 15, 10, 13, 14, 1, 7, 4, 12, 9, 6, 0, 171, 163, 165, 168, 162, 175, 170, 173, 174, 161, 167, 164, 172, 169, 166, 160, 91, 83, 85, 88, 82, 95, 90, 93, 94, 81, 87, 84, 92, 89, 86, 80, 59, 51, 53, 56, 50, 63, 58, 61, 62, 49, 55, 52, 60, 57, 54, 48, 235, 227, 229, 232, 226, 239, 234, 237, 238, 225, 231, 228, 236, 233, 230, 224, 155, 147, 149, 152, 146, 159, 154, 157, 158, 145, 151, 148, 156, 153, 150, 144, 187, 179, 181, 184, 178, 191, 186, 189, 190, 177, 183, 180, 188, 185, 182, 176 },
+{ 87, 95, 85, 90, 88, 81, 86, 93, 80, 89, 83, 94, 91, 84, 82, 92, 215, 223, 213, 218, 216, 209, 214, 221, 208, 217, 211, 222, 219, 212, 210, 220, 247, 255, 245, 250, 248, 241, 246, 253, 240, 249, 243, 254, 251, 244, 242, 252, 103, 111, 101, 106, 104, 97, 102, 109, 96, 105, 99, 110, 107, 100, 98, 108, 151, 159, 149, 154, 152, 145, 150, 157, 144, 153, 147, 158, 155, 148, 146, 156, 39, 47, 37, 42, 40, 33, 38, 45, 32, 41, 35, 46, 43, 36, 34, 44, 199, 207, 197, 202, 200, 193, 198, 205, 192, 201, 195, 206, 203, 196, 194, 204, 167, 175, 165, 170, 168, 161, 166, 173, 160, 169, 163, 174, 171, 164, 162, 172, 183, 191, 181, 186, 184, 177, 182, 189, 176, 185, 179, 190, 187, 180, 178, 188, 119, 127, 117, 122, 120, 113, 118, 125, 112, 121, 115, 126, 123, 116, 114, 124, 135, 143, 133, 138, 136, 129, 134, 141, 128, 137, 131, 142, 139, 132, 130, 140, 23, 31, 21, 26, 24, 17, 22, 29, 16, 25, 19, 30, 27, 20, 18, 28, 71, 79, 69, 74, 72, 65, 70, 77, 64, 73, 67, 78, 75, 68, 66, 76, 55, 63, 53, 58, 56, 49, 54, 61, 48, 57, 51, 62, 59, 52, 50, 60, 231, 239, 229, 234, 232, 225, 230, 237, 224, 233, 227, 238, 235, 228, 226, 236, 7, 15, 5, 10, 8, 1, 6, 13, 0, 9, 3, 14, 11, 4, 2, 12 },
+{ 24, 30, 18, 21, 22, 25, 17, 28, 31, 20, 27, 16, 29, 26, 19, 23, 120, 126, 114, 117, 118, 121, 113, 124, 127, 116, 123, 112, 125, 122, 115, 119, 232, 238, 226, 229, 230, 233, 225, 236, 239, 228, 235, 224, 237, 234, 227, 231, 216, 222, 210, 213, 214, 217, 209, 220, 223, 212, 219, 208, 221, 218, 211, 215, 8, 14, 2, 5, 6, 9, 1, 12, 15, 4, 11, 0, 13, 10, 3, 7, 88, 94, 82, 85, 86, 89, 81, 92, 95, 84, 91, 80, 93, 90, 83, 87, 136, 142, 130, 133, 134, 137, 129, 140, 143, 132, 139, 128, 141, 138, 131, 135, 56, 62, 50, 53, 54, 57, 49, 60, 63, 52, 59, 48, 61, 58, 51, 55, 72, 78, 66, 69, 70, 73, 65, 76, 79, 68, 75, 64, 77, 74, 67, 71, 248, 254, 242, 245, 246, 249, 241, 252, 255, 244, 251, 240, 253, 250, 243, 247, 168, 174, 162, 165, 166, 169, 161, 172, 175, 164, 171, 160, 173, 170, 163, 167, 104, 110, 98, 101, 102, 105, 97, 108, 111, 100, 107, 96, 109, 106, 99, 103, 152, 158, 146, 149, 150, 153, 145, 156, 159, 148, 155, 144, 157, 154, 147, 151, 200, 206, 194, 197, 198, 201, 193, 204, 207, 196, 203, 192, 205, 202, 195, 199, 184, 190, 178, 181, 182, 185, 177, 188, 191, 180, 187, 176, 189, 186, 179, 183, 40, 46, 34, 37, 38, 41, 33, 44, 47, 36, 43, 32, 45, 42, 35, 39 },
+};
+
+
+__device__ half_block_t invert(const half_block_t block) {
+    half_block_t result;
+    for (int i = 0; i < 4; ++i) {
+        result.bytes[i] = block.bytes[3 - i];
+    }
+    return result;
+}
 
 __device__ uint32_t Add(const half_block_t a, const half_block_t b) {
     return a.uint ^ b.uint;
 }
 
-__device__ half_block_t Add32(half_block_t a, half_block_t b) {
-    //a = a.invert_bytes_order();
-    //b = b.invert_bytes_order();
-    a.uint = a.uint + b.uint;
-    //return a.invert_bytes_order();
-    return a;
+__device__ uint32_t Add32(const half_block_t a, const half_block_t b) {
+    return a.uint + b.uint;
+    /*half_block_t c, internal;
+    for (int i = 3; i >= 0; --i)
+    {
+        internal = a.bytes[i] + b.bytes[i] + (internal.bytes >> 8);
+        c[i] = internal & 0xff;
+    }*/
 }
 
 //__host__ void ExpandKey(const uint8_t* key)
@@ -122,20 +138,24 @@ __device__ half_block_t Add32(half_block_t a, half_block_t b) {
 //}
 __device__ half_block_t magma_T(const half_block_t data) {
     half_block_t result;
-    uint8_t first_part_byte, sec_part_byte;
+    /*uint8_t first_part_byte, sec_part_byte;
     for (int i = 0; i < 4; ++i) {
         first_part_byte = data.bytes[i] >> 4;
         sec_part_byte = (data.bytes[i] & 0x0f);
         first_part_byte = table[i * 2][first_part_byte];
         sec_part_byte = table[i * 2 + 1][sec_part_byte];
         result.bytes[i] = (first_part_byte << 4) | sec_part_byte;
+    }*/
+    for (int i = 0; i < 4; ++i) {
+        result.bytes[i] = table4x256[i][data.bytes[i]];
     }
     return result;
 }
 
 __device__ half_block_t magma_g(const key_t round_key, const half_block_t block) {
     half_block_t internal = block;
-    internal = Add32(internal, round_key);
+    //half_block_t key = invert(round_key);
+    internal.uint = Add32(block, round_key);
     internal = magma_T(internal);
     internal.uint = (internal.uint << 11) | (internal.uint >> 21);
 
@@ -164,11 +184,11 @@ __device__ block_t magma_G_last(const key_t round_key, const block_t block)
     return result;
 }
 
-__global__ void Encrypt (const key_set& round_key, const block_t& block, block_t& result_block)
+__global__ void Encrypt(const key_set& round_key, const block_t& block, block_t& result_block)
 {
     // Первое преобразование G
-    block_t result = { block.lo.invert_bytes_order(), block.hi.invert_bytes_order() };
-    result = magma_G(round_key.keys[0], result);
+    block_t result;
+    result = magma_G(round_key.keys[0], block);
     // Последующие (со второго по тридцать первое) преобразования G
     for (int i = 1; i < 24; ++i)
         result = magma_G(round_key.keys[i % 8], result);
@@ -176,73 +196,29 @@ __global__ void Encrypt (const key_set& round_key, const block_t& block, block_t
         result = magma_G(round_key.keys[7 - i % 8], result);
     // Последнее (тридцать второе) преобразование G
     result = magma_G_last(round_key.keys[0], result);
-    result_block = { result.lo.invert_bytes_order(), result.hi.invert_bytes_order() };
+    result_block = result;
 }
 
 
-__global__ void Decrypt (const key_set& round_key, const block_t& block, block_t& result_block)
+__global__ void Decrypt(const key_set& round_key, const block_t& block, block_t& result_block)
 {
-    block_t result = { block.lo.invert_bytes_order(), block.hi.invert_bytes_order() };;
-    result = magma_G(round_key.keys[0], result);
+    block_t result;
+    result = magma_G(round_key.keys[0], block);
     for (int i = 1; i < 8; ++i)
         result = magma_G(round_key.keys[i % 8], result);
     for (int i = 0; i < 23; ++i)
         result = magma_G(round_key.keys[7 - i % 8], result);
     // Последнее (тридцать второе) преобразование G
     result = magma_G_last(round_key.keys[0], result);
-    result_block = { result.lo.invert_bytes_order(), result.hi.invert_bytes_order() };
+    result_block = result;
 }
 
 
-void encryptCuda(const uint8_t* block, uint8_t* out_block, const key_set& keys);
-void decryptCuda(const uint8_t* block, uint8_t* out_block, const key_set& keys);
-
-/** @brief Конвертирует массив байт в int32
- *
- * @param[in] input массив из 4 байт
- * @return int32 число
- */
-static unsigned int uint8ToUint32(unsigned char* input)
-{
-    unsigned int r = ((input[3]) | (input[2] << 8) | (input[1] << 16) | (input[0] << 24));
-    return r;
-}
-
-/** @brief Конвертирует int32 в массив байт
- *
- * @param[in] input int32 число
- * @param[out] output массив из 4 байт
- */
-static void uint32ToUint8(unsigned int input, unsigned char* output)
-{
-    int i;
-    for (i = 0; i < 4; ++i)
-    {
-        output[3 - i] = ((input >> (8 * i)) & 0x000000ff);
-    }
-}
-
-int main()
-{
-    // SetConsoleOutputCP(1251);
-    // SetConsoleCP(1251);
-    SetConsoleOutputCP(65001);
-    block_t en_blk, encrypt_block;
-    const unsigned char encrypt_test_string[8] = {
-        0x10, 0x32, 0x54, 0x76, 0x98, 0xba, 0xdc, 0xfe
-    };
-    /*const unsigned char encrypt_test_string[8] = {
-        0xfe, 0xdc, 0xba, 0x98, 0x76, 0x54, 0x32, 0x10
-    };*/
-    memccpy(en_blk.bytes, encrypt_test_string, 8, 8);
+void encryptCuda(const uint8_t* block, uint8_t* out_block, const size_t blockSize, const key_set& keys);
+void decryptCuda(const uint8_t* block, uint8_t* out_block, const size_t blockSize, const key_set& keys);
 
 
-    const unsigned char ecrypt_string[8] = {
-        0x3d, 0xca, 0xd8, 0xc2, 0xe5, 0x01, 0xe9, 0x4e
-    };
-    memccpy(encrypt_block.bytes, ecrypt_string, 8, 8);
-
-    const key_set keys = {
+const key_set keys = {
         key_t {0xcc, 0xdd, 0xee, 0xff},
         key_t {0x88, 0x99, 0xaa, 0xbb},
         key_t {0x44, 0x55, 0x66, 0x77},
@@ -251,18 +227,41 @@ int main()
         key_t {0xf7, 0xf6, 0xf5, 0xf4},
         key_t {0xfb, 0xfa, 0xf9, 0xf8},
         key_t {0xff, 0xfe, 0xfd, 0xfc}
-    };
+};
 
-    /*const key_set keys = {
-        key_t {0xff, 0xfe, 0xfd, 0xfc},
-        key_t {0xfb, 0xfa, 0xf9, 0xf8},
-        key_t {0xf7, 0xf6, 0xf5, 0xf4},
-        key_t {0xf3, 0xf2, 0xf1, 0xf0},
-        key_t {0x00, 0x11, 0x22, 0x33},
-        key_t {0x44, 0x55, 0x66, 0x77},        
-        key_t {0x88, 0x99, 0xaa, 0xbb},
-        key_t {0xcc, 0xdd, 0xee, 0xff}
-    };*/
+void test() {
+    constexpr size_t size = 1024 * 1024 * 1024;
+    std::vector<block_t> data(size / sizeof(block_t));
+    uint32_t i = 0;
+    for (auto& b : data) b.ull = ++i << 32 | ++i;
+
+    using duration = std::chrono::duration<double, std::milli>;
+    auto start = std::chrono::high_resolution_clock::now();
+    encryptCuda((uint8_t*)data.data(), (uint8_t*)data.data(), size, keys);
+    duration time = std::chrono::high_resolution_clock::now() - start;
+    std::cout << "SIZE: " << size << "\tTIME: " << time.count() << "ms\t SPEED" << (size / 1024.0 / 1024 / 1024) / time.count() * 1000 << " GB/s" << std::endl;
+
+}
+
+
+int main()
+{
+    // SetConsoleOutputCP(1251);
+    // SetConsoleCP(1251);
+    SetConsoleOutputCP(65001);
+    block_t en_blk, enc_e_t;
+    const unsigned char encrypt_test_string[8] = {
+        0x10, 0x32, 0x54, 0x76, 0x98, 0xba, 0xdc, 0xfe
+    };
+    memccpy(en_blk.bytes, encrypt_test_string, 8, 8);
+    const unsigned char encrypt_exaple_text[8] = {
+        0x3d, 0xca, 0xd8, 0xc2, 0xe5, 0x01, 0xe9, 0x4e
+    };
+    memccpy(enc_e_t.bytes, encrypt_exaple_text, 8, 8);
+
+    std::cout << sizeof(en_blk) << std::endl;
+
+
 
     cudaDeviceProp prop;
     int count;
@@ -306,23 +305,22 @@ int main()
     // ExpandKey(keys);
     std::cout << "encrypt_test_string: " << en_blk << std::endl;
 
-    encryptCuda(encrypt_test_string, resultEncrypt.bytes, keys);
+    encryptCuda(encrypt_test_string, resultEncrypt.bytes, 8, keys);
 
 
     //printf("Encryption text: {%s}\n", result);
     std::cout << "Encryption text: " << resultEncrypt << std::endl;
-    std::cout << "Standart_encrypt_text: " << encrypt_block << std::endl;
+    std::cout << "Encr examp text: " << enc_e_t << std::endl;
 
     block_t resultDecrypt{};
 
-    decryptCuda(resultEncrypt.bytes, resultDecrypt.bytes, keys);
+    decryptCuda(resultEncrypt.bytes, resultDecrypt.bytes, 8, keys);
 
     //printf("Encryption text: {%s}\n", result);
-    //resultDecrypt.hi.uint = uint8ToUint32(resultDecrypt.hi.bytes);
-    //resultDecrypt.lo.uint = uint8ToUint32(resultDecrypt.lo.bytes);
     std::cout << "Decryption text: " << resultDecrypt << std::endl;
 
-    cudaCheck(cudaDeviceReset());
+    test();
+
     return 0;
 }
 
@@ -348,7 +346,7 @@ cuda_ptr<T[]> cuda_alloc(size_t n) {
     return cuda_ptr<T[]>{ptr};
 }
 
-void encryptCuda(const uint8_t* block, uint8_t* out_block, const key_set& keys) {
+void encryptCuda(const uint8_t* block, uint8_t* out_block, const size_t blockSize, const key_set& keys) {
     cuda_ptr<key_set> dev_keys = cuda_alloc<key_set>();
     cuda_ptr<block_t> dev_block = cuda_alloc<block_t>();
     cuda_ptr<block_t> dev_out_block = cuda_alloc<block_t>();
@@ -376,7 +374,7 @@ void encryptCuda(const uint8_t* block, uint8_t* out_block, const key_set& keys) 
 }
 
 
-void decryptCuda(const uint8_t* block, uint8_t* out_block, const key_set& keys) {
+void decryptCuda(const uint8_t* block, uint8_t* out_block, const size_t blockSize, const key_set& keys) {
     cuda_ptr<key_set> dev_keys = cuda_alloc<key_set>();
     cuda_ptr<block_t> dev_block = cuda_alloc<block_t>();
     cuda_ptr<block_t> dev_out_block = cuda_alloc<block_t>();
@@ -384,6 +382,8 @@ void decryptCuda(const uint8_t* block, uint8_t* out_block, const key_set& keys) 
     cudaError_t cudaStatus;
 
     cudaCheck(cudaSetDevice(0));
+    std::cout << dev_block.get() << std::endl;
+
 
     cudaCheck(cudaMemcpy(dev_keys.get(), keys.keys, sizeof(key_set), cudaMemcpyHostToDevice));
 
@@ -393,14 +393,12 @@ void decryptCuda(const uint8_t* block, uint8_t* out_block, const key_set& keys) 
 
     Decrypt << <1, 1 >> > (*dev_keys, *dev_block, *dev_out_block);
 
+    // cuddaGetLastError
     cudaCheck(cudaDeviceSynchronize());
 
     cudaCheck(cudaMemcpy(out_block, dev_out_block.get(), sizeof(block_t), cudaMemcpyDeviceToHost));
 
-Error:
-    dev_block.get_deleter();
-    dev_out_block.get_deleter();
-    dev_keys.get_deleter();
+
 
 
     // return cudaStatus;
@@ -410,4 +408,5 @@ Error:
 
 // text: fedcba9876543210
 // encr_text: 4ee901e5c2d8ca3d
+// gipher_text: 4ee901e5c2d8ca3d
 // dec_text: fedcba9876543210
